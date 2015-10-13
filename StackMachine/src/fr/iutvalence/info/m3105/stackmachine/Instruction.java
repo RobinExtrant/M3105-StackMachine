50e2eca94593e06aa4f3c09a5b2dad771b39d6af
extrant.robin <robin.extrant@iut-valence.fr>
2015-10-13 08:32:10 +0200

    add constructor and method getParams and getOpCode in class Instruction

--- StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Instruction.java
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Instruction.java
@@ -2,4 +2,18 @@ package fr.iutvalence.info.m3105.stackmachine;
 
 public class Instruction {
 
+	public Instruction(int call, int[] is) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int[] getParams() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getOpCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
